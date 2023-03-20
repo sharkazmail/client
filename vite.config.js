e62491a1ed9be86ec0779ff64a11455ddbbaaa94
Femi Johnson <ogunbanwofemi2000@gmail.com>
2023-03-20 20:17:55 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api-git-master-johnson12344.vercel.app/',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
